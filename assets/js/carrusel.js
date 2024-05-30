93e60a0e68dbb2452137974a3edb6cfb0940c139
AndyLaffertt <alaffertt@gmail.com>
2024-05-30 15:47:09 -0400

    más imagenes y arreglos en galeria

--- assets/js/carrusel.js
+++ assets/js/carrusel.js
@@ -9,3 +9,27 @@ function showNextItem() {
 }
 
 setInterval(showNextItem, 3000); // Cambia de imagen cada 3 segundos
+
+document.addEventListener('DOMContentLoaded', function() {
+    const tabs = document.querySelectorAll('.tabs a');
+    const mediaItems = document.querySelectorAll('.media');
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function(event) {
+            event.preventDefault();
+            const tag = this.getAttribute('data-tag');
+
+            tabs.forEach(t => t.classList.remove('active'));
+            this.classList.add('active');
+
+            mediaItems.forEach(item => {
+                if (tag === 'all' || item.classList.contains(tag)) {
+                    item.style.display = 'block';
+                } else {
+                    item.style.display = 'none';
+                }
+            });
+        });
+    });
+});
+
